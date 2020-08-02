f06f91ab5feafc0104369181588eca8b4d0b9a1d
kwojcik342 <krzysztof2.wojcik@gmail.com>
2020-08-02 19:37:22 +0200

    added web app

---
+++ spendings_tracker_flask/web_app/db_opers.py
@@ -0,0 +1,233 @@
+import psycopg2
+
+
+def get_user_id(in_username):
+    q_select_user = "select id_user from users where username=(%s)"
+    id_user = -1
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_select_user, (in_username,))
+        id_user = db_cur.fetchone()[0]
+        print("selected user id: " + str(id_user))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_user
+
+
+def insert_user(in_username):
+    q_insert_usr = "insert into users (username) values (%s) returning id_user;"
+    id_user = -1
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_insert_usr, (in_username,))
+        id_user = db_cur.fetchone()[0]
+        db_con.commit()
+        print("Added new user with id= " + str(id_user))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_user
+
+
+def insert_income(in_id_user, in_inc_amount, in_inc_tmstmp, in_income_note):
+    q_insert_inc = "insert into user_incomes (id_user, income_amount, income_timestamp, income_user_note) values (%s,%s,%s,%s) returning id_income;"
+    id_income = -1
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_insert_inc, (in_id_user, in_inc_amount, in_inc_tmstmp, in_income_note,))
+        id_income = db_cur.fetchone()[0]
+        db_con.commit()
+        print("Added new income with id= " + str(id_income))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_income
+
+
+def update_income(in_id_income, in_inc_amount, in_inc_tmstmp, in_income_note):
+    q_update_inc = "update user_incomes set income_amount=%s, income_timestamp=%s, income_user_note=%s where id_income=%s;"
+    id_income = -1
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_update_inc, (in_inc_amount, in_inc_tmstmp, in_income_note, in_id_income))
+        id_income = 1
+        db_con.commit()
+        print("Updated income with id= " + str(in_id_income))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_income
+
+
+def delete_incomes(in_inc_list):
+    q_delete_inc = "delete from user_incomes where id_income =%s;"
+    id_income = -1
+    db_con = None
+    inc_list = []
+    for inc in in_inc_list:
+        inc_list.append(tuple((inc, )))
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.executemany(q_delete_inc, inc_list)
+        id_income = 1
+        db_con.commit()
+        print("Deleted incomes with id= " + str(in_inc_list))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_income
+
+
+def get_user_incomes(in_id_user):
+    q_sel_inc = "select id_income, income_amount, income_timestamp, income_user_note from user_incomes where id_user = %s order by income_timestamp desc;"
+    incomes_list = []
+    l_resp = []
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_sel_inc, (in_id_user,))
+        incomes_list = db_cur.fetchall()
+
+        l_inc_js_tags = ["id_income", "inc_amount", "inc_tmst", "inc_note"]
+        for inc in incomes_list:
+            l_tmp = [inc[0], float(inc[1]), inc[2].strftime("%Y-%m-%d %H:%M:%S"), inc[3]]
+            d_tmp = dict(zip(l_inc_js_tags, l_tmp))
+            l_resp.append(d_tmp)
+
+        print("Selected " + str(db_cur.rowcount) + " incomes where id_user= " + str(in_id_user))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return l_resp
+
+
+def insert_spending(in_id_user, in_sp_amount, in_sp_tmstmp, in_sp_note):
+    q_insert_sp = "insert into user_spendings (id_user, spending_amount, spending_timestamp, spending_user_note) values (%s,%s,%s,%s) returning id_spending;"
+    id_spending = -1
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_insert_sp, (in_id_user, in_sp_amount, in_sp_tmstmp, in_sp_note,))
+        id_spending = db_cur.fetchone()[0]
+        db_con.commit()
+        print("Added new spending with id= " + str(id_spending))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_spending
+
+
+def update_spending(in_id_spending, in_sp_amount, in_sp_tmstmp, in_sp_note):
+    q_update_sp = "update user_spendings set spending_amount=%s, spending_timestamp=%s, spending_user_note=%s where id_spending=%s;"
+    id_spending = -1
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.execute(q_update_sp, (in_sp_amount, in_sp_tmstmp, in_sp_note, in_id_spending,))
+        id_spending = 1
+        db_con.commit()
+        print("Updated spending with id= " + str(in_id_spending))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_spending
+
+
+def delete_spendings(in_sp_list):
+    q_delete_sp = "delete from user_spendings where id_spending =%s;"
+    id_spending = -1
+    db_con = None
+    sp_list = []
+    for sp in in_sp_list:
+        sp_list.append(tuple((sp, )))
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+        db_cur.executemany(q_delete_sp, sp_list)
+        id_spending = 1
+        db_con.commit()
+        print("Deleted spendings with id= " + str(in_sp_list))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return id_spending
+
+
+def get_user_spendings(in_id_user, in_start_date, in_end_date):
+    q_sel_sp = "select id_spending, spending_amount, spending_timestamp, spending_user_note from user_spendings where id_user = %s order by spending_timestamp asc;"
+    q_sel_sp_sd = "select id_spending, spending_amount, spending_timestamp, spending_user_note from user_spendings where id_user = %s and spending_timestamp >= %s order by spending_timestamp asc;"
+    q_sel_sp_ed = "select id_spending, spending_amount, spending_timestamp, spending_user_note from user_spendings where id_user = %s and spending_timestamp <= %s order by spending_timestamp asc;"
+    q_sel_sp_md = "select id_spending, spending_amount, spending_timestamp, spending_user_note from user_spendings where id_user = %s and spending_timestamp >= %s and spending_timestamp <= %s order by spending_timestamp asc;"
+
+    spendings_list = []
+    l_resp = []
+    db_con = None
+    try:
+        db_con = psycopg2.connect(host="localhost", database="SpendingTracker_DB", user="st_server_db_user", password="user123")
+        db_cur = db_con.cursor()
+
+        if in_start_date != "" and in_end_date != "":
+            db_cur.execute(q_sel_sp_md, (in_id_user, in_start_date, in_end_date,))
+        elif in_start_date != "" and in_end_date == "":
+            db_cur.execute(q_sel_sp_sd, (in_id_user, in_start_date,))
+        elif in_start_date == "" and in_end_date != "":
+            db_cur.execute(q_sel_sp_ed, (in_id_user, in_end_date,))
+        else:
+            db_cur.execute(q_sel_sp, (in_id_user,))
+
+        spendings_list = db_cur.fetchall()
+
+        l_sp_js_tags = ["id_spending", "sp_amount", "sp_tmst", "sp_note"]
+        for sp in spendings_list:
+            l_tmp = [sp[0], float(sp[1]), sp[2].strftime("%Y-%m-%d %H:%M:%S"), sp[3]]
+            d_tmp = dict(zip(l_sp_js_tags, l_tmp))
+            l_resp.append(d_tmp)
+
+        print("Selected " + str(db_cur.rowcount) + " spendings where id_user= " + str(in_id_user) + " between dates: " + str(in_start_date) + " and " + str(in_end_date))
+        db_cur.close()
+    except psycopg2.DatabaseError as err:
+        print(err)
+    finally:
+        if db_con is not None:
+            db_con.close()
+        return l_resp
---
+++ spendings_tracker_flask/web_app/flask_web_app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import db_opers as dbo
+import pygal as pg
+
+
+app = Flask(__name__)
+app.secret_key = "test1"
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        usern = request.values["inputLogin"]
+        id_usr = dbo.get_user_id(usern)
+        session["id_user"] = id_usr
+        if id_usr < 0:
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("user_page"))
+    else:
+        if "id_user" in session:
+            return redirect(url_for("user_page"))
+        else:
+            return render_template("index.html")
+
+
+@app.route("/user_page")
+def user_page():
+    if "id_user" in session:
+        id_user = session["id_user"]
+        if request.method == "POST":
+            return render_template("user_page.html", content="post")
+        else:
+            usr_inc = dbo.get_user_incomes(id_user)
+
+            max_inc_num = 11
+            start_date = ""
+            end_date = ""
+            l_months =[]
+            l_inc = []
+            l_sp_sum = []
+            for index, inc in enumerate(usr_inc):
+                if index > max_inc_num:
+                    break
+                # print(index, inc) 
+                inc_amount = 0
+                sp_sum = 0
+                end_date = start_date
+                start_date = inc["inc_tmst"]
+                month = int(start_date[5:7])+1
+                if month == 13:
+                    month = 1
+                inc_amount = inc["inc_amount"] 
+                d_sp = dbo.get_user_spendings(3, start_date, end_date)
+                for sp in d_sp:
+                    sp_sum += sp["sp_amount"]
+
+                l_months.append(month)
+                l_inc.append(inc_amount)
+                l_sp_sum.append(sp_sum)
+
+            monthly_chart = pg.Bar()
+            monthly_chart.x_labels = map(str, l_months)
+            monthly_chart.add("income", l_inc)
+            monthly_chart.add("spendings", l_sp_sum)
+            chart_m_data = monthly_chart.render_data_uri()
+
+            return render_template("user_page.html"
+                                    , chart=chart_m_data
+                                    , j_incomes=usr_inc)
+    else:
+        return redirect(url_for("home"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("id_user", None)
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
+    app.run()
