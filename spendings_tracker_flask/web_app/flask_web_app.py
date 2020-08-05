556d9834cbc428f51f024623eaf44ae657cffa34
kwojcik342 <krzysztof2.wojcik@gmail.com>
2020-08-05 09:46:00 +0200

    added remaining functionality for tracking spendings

--- spendings_tracker_flask/web_app/flask_web_app.py
+++ spendings_tracker_flask/web_app/flask_web_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import db_opers as dbo
 import pygal as pg
+from datetime import date, datetime
 
 
 app = Flask(__name__)
@@ -29,6 +30,7 @@ def user_page():
     if "id_user" in session:
         id_user = session["id_user"]
 
+        # handling deletes and inserts
         if request.method == "POST":
             if "fin_inc_tmst" in request.values:
                 tmp_tmst = request.values["fin_inc_tmst"]
@@ -36,15 +38,29 @@ def user_page():
                 res = dbo.insert_income(id_user, request.values["fin_inc_am"], tmp_tmst, request.values["fin_inc_usr_note"])
                 if res < 0:
                     pass
-            if "id_inc_del" in request.values:
+            if "fin_id_inc_del" in request.values:
                 l_id_inc_del = []
-                l_id_inc_del.append(int(request.values["id_inc_del"]))
+                l_id_inc_del.append(int(request.values["fin_id_inc_del"]))
                 res = dbo.delete_incomes(l_id_inc_del)
                 if res < 0:
-                    pass            
+                    pass   
+            if "fin_sp_tmst" in request.values:
+                tmp_tmst = request.values["fin_sp_tmst"]
+                tmp_tmst = tmp_tmst[0:10] + " " + tmp_tmst[11:] + ":00"
+                res = dbo.insert_spending(id_user, request.values["fin_sp_am"], tmp_tmst, request.values["fin_sp_usr_note"])
+                if res < 0:
+                    pass
+            if "fin_id_sp_del" in request.values:
+                l_id_inc_del = []
+                l_id_inc_del.append(int(request.values["fin_id_sp_del"]))
+                res = dbo.delete_spendings(l_id_inc_del)
+                if res < 0:
+                    pass     
 
+        # we always get some data from db so the page doesn't appear empty 
         usr_inc = dbo.get_user_incomes(id_user)
 
+        # getting data for chart comparing spendings to incomes in past 12 months
         max_inc_num = 11
         start_date = ""
         end_date = ""
@@ -77,23 +93,66 @@ def user_page():
         monthly_chart.add("spendings", l_sp_sum)
         chart_m_data = monthly_chart.render_data_uri()
 
+        # getting data for chart comparing spendings to incomes
+        # if user selected incomes from table we compare spendings between min and max date of selected incomes
+        # if only one date was selected by user we show comparison between that date and current date
+        # if no date is selected or it's GET request we show spendings after latest income and compare them to latest income
         if request.method == "POST" and "spCheckbox" in request.form:
             boxSelected = request.form.getlist("spCheckbox")
             if len(boxSelected) == 1:
-                l_usr_spendings = dbo.get_user_spendings(id_user, boxSelected[0], "")
+                for inc in usr_inc:
+                    if inc["id_income"] == int(boxSelected[0]):
+                        sp_st_date = inc["inc_tmst"]
+                        sp_end_date = ""
+                        break
             elif len(boxSelected) > 1:
-                boxSelected.sort()
-                l_usr_spendings = dbo.get_user_spendings(id_user, boxSelected[0], boxSelected[-1])
-            print(l_usr_spendings)
+                l_sel_inc_dates = []
+                for chb in boxSelected:
+                    for inc in usr_inc:
+                        if inc["id_income"] == int(chb):
+                            l_sel_inc_dates.append(datetime.strptime(inc["inc_tmst"], "%Y-%m-%d %H:%M:%S"))
+                l_sel_inc_dates.sort()
+                sp_st_date = str(l_sel_inc_dates[0])
+                sp_end_date = str(l_sel_inc_dates[-1])
         elif request.method == "GET" or (request.method == "POST" and "spCheckbox" not in request.form):
-            default_sp_st_date = usr_inc[0]["inc_tmst"]
-            l_usr_spendings = dbo.get_user_spendings(id_user, default_sp_st_date, "")
-            print(l_usr_spendings)
+            sp_st_date = usr_inc[0]["inc_tmst"]
+            sp_end_date = ""
 
+        l_usr_spendings = dbo.get_user_spendings(id_user, sp_st_date, sp_end_date)
+
+        # summing up spendings amounts
+        sum_sp = 0.00
+        l_sum_sp = []
+        for spd in l_usr_spendings:
+            sum_sp += spd["sp_amount"]
+        l_sum_sp.append(sum_sp)
+
+        # if user selected only one date we set end date as today so the label on the chart isn't empty
+        if sp_end_date == "":
+            sp_end_date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+
+        # summing up incomes between selected dates
+        sum_inc = 0.00
+        l_sum_inc = []
+        dt_start = datetime.strptime(sp_st_date, "%Y-%m-%d %H:%M:%S")
+        dt_end = datetime.strptime(sp_end_date, "%Y-%m-%d %H:%M:%S")
+        for inc in usr_inc:
+            dt_inc = datetime.strptime(inc["inc_tmst"], "%Y-%m-%d %H:%M:%S")
+            if dt_inc >= dt_start and dt_inc < dt_end:
+                sum_inc += inc["inc_amount"]
+        l_sum_inc.append(sum_inc)
+
+        sp2inc_chart = pg.Bar()
+        sp2inc_chart.x_labels = [sp_st_date + " - " + sp_end_date]
+        sp2inc_chart.add("incomes", l_sum_inc)
+        sp2inc_chart.add("spendings", l_sum_sp)
+        chart_sp2inc_data = sp2inc_chart.render_data_uri()
 
         return render_template("user_page.html"
-                                , chart=chart_m_data
-                                , j_incomes=usr_inc)
+                                , chartinc=chart_m_data
+                                , j_incomes=usr_inc
+                                , j_spendings=l_usr_spendings
+                                , chartsp2inc=chart_sp2inc_data)
     else:
         return redirect(url_for("home"))
 
